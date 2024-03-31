54009048efec3576ec51512aa4d6cd42bc285f2c
misasoares <misasoaresms2@gmail.com>
2024-03-31 17:08:14 -0300

    feature: adiciona filtro de pesquisa

--- src/app/main/checks/components/ChecksTable.tsx
+++ src/app/main/checks/components/ChecksTable.tsx
@@ -1,11 +1,98 @@
+// import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
+// import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+// import { useAppSelector } from 'app/store/hooks';
+
+// export default function ChecksTable() {
+// 	const checks = useAppSelector((state) => state.checks);
+
+// 	function formatDate(dateParam: Date) {
+// 		const date = new Date(dateParam);
+// 		const day = date.getDate();
+// 		const month = date.getMonth() + 1;
+// 		const year = date.getFullYear();
+
+// 		return `${day}/${month}/${year}`;
+// 	}
+
+// 	return (
+// 		<div className="p-32">
+// 			<Paper
+// 				elevation={4}
+// 				className="p-32 mt-32"
+// 			>
+// 				<TableContainer component={Paper}>
+// 					<Table
+// 						sx={{ minWidth: 650 }}
+// 						size="small"
+// 						aria-label="a dense table"
+// 					>
+// 						<TableHead>
+// 							<TableRow>
+// 								<TableCell>Pagador</TableCell>
+// 								<TableCell>Conta</TableCell>
+// 								<TableCell>Valor</TableCell>
+// 								<TableCell>Vencimento</TableCell>
+// 								<TableCell>Ações</TableCell>
+// 							</TableRow>
+// 						</TableHead>
+// 						<TableBody>
+// 							{checks.checks.map((row) => (
+// 								<TableRow
+// 									key={row.uid}
+// 									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+// 								>
+// 									<TableCell>{row.payerName}</TableCell>
+// 									<TableCell
+// 										component="th"
+// 										scope="row"
+// 									>
+// 										{row.AccountBank.name}
+// 									</TableCell>
+// 									<TableCell>{row.value}</TableCell>
+// 									<TableCell>{formatDate(row.dueDate)}</TableCell>
+// 									<TableCell className="flex">
+// 										<FuseSvgIcon>heroicons-outline:pencil</FuseSvgIcon>
+// 										<FuseSvgIcon>heroicons-outline:eye</FuseSvgIcon>
+// 									</TableCell>
+// 								</TableRow>
+// 							))}
+// 						</TableBody>
+// 					</Table>
+// 				</TableContainer>
+// 			</Paper>
+// 		</div>
+// 	);
+// }
+
 import FuseSvgIcon from '@fuse/core/FuseSvgIcon';
-import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import {
+	Button,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	TextField
+} from '@mui/material';
+import { useState } from 'react';
 import { useAppSelector } from 'app/store/hooks';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { ptBR } from 'date-fns/locale';
 
 export default function ChecksTable() {
 	const checks = useAppSelector((state) => state.checks);
+	const [filters, setFilters] = useState({
+		accountBankName: '',
+		dueDate: null,
+		payerName: '',
+		value: '',
+		checkNumber: ''
+	});
 
-	function formatDate(dateParam: Date) {
+	function formatDate(dateParam) {
 		const date = new Date(dateParam);
 		const day = date.getDate();
 		const month = date.getMonth() + 1;
@@ -14,12 +101,85 @@ export default function ChecksTable() {
 		return `${day}/${month}/${year}`;
 	}
 
+	function applyFilters(row) {
+		return (
+			row.AccountBank.name.toLowerCase().includes(filters.accountBankName.toLowerCase()) &&
+			(filters.dueDate === null || new Date(row.dueDate).getTime() === filters.dueDate.getTime()) &&
+			row.payerName.toLowerCase().includes(filters.payerName.toLowerCase()) &&
+			String(row.value).toLowerCase().includes(filters.value.toLowerCase()) &&
+			row.checkNumber.toLowerCase().includes(filters.checkNumber.toLowerCase())
+		);
+	}
+
+	function clearFilters() {
+		setFilters({
+			accountBankName: '',
+			dueDate: null,
+			payerName: '',
+			value: '',
+			checkNumber: ''
+		});
+	}
+
 	return (
 		<div className="p-32">
 			<Paper
 				elevation={4}
 				className="p-32 mt-32"
 			>
+				<div className="flex gap-2 justify-between flex-wrap items-center mb-16">
+					<TextField
+						label="Conta"
+						value={filters.accountBankName}
+						onChange={(e) => setFilters({ ...filters, accountBankName: e.target.value })}
+					/>
+					<TextField
+						label="Pagador"
+						value={filters.payerName}
+						onChange={(e) => setFilters({ ...filters, payerName: e.target.value })}
+					/>
+
+					<TextField
+						label="Número do cheque"
+						value={filters.checkNumber}
+						onChange={(e) => setFilters({ ...filters, checkNumber: e.target.value })}
+					/>
+
+					<TextField
+						label="Valor"
+						value={filters.value}
+						onChange={(e) => setFilters({ ...filters, value: e.target.value })}
+					/>
+
+					<LocalizationProvider
+						dateAdapter={AdapterDateFns}
+						adapterLocale={ptBR}
+					>
+						<DatePicker
+							label="Data de vencimento"
+							format="dd/MM/yyyy"
+							value={filters.dueDate}
+							onChange={(date) => setFilters({ ...filters, dueDate: date })}
+							sx={{
+								'& .MuiFormHelperText-root': {
+									position: 'absolute',
+									top: '55px'
+								},
+								width: 227
+							}}
+						/>
+					</LocalizationProvider>
+				</div>
+
+				<div className="w-full flex justify-end">
+					<Button
+						onClick={clearFilters}
+						variant="outlined"
+					>
+						Limpar filtros
+					</Button>
+				</div>
+
 				<TableContainer component={Paper}>
 					<Table
 						sx={{ minWidth: 650 }}
@@ -30,13 +190,14 @@ export default function ChecksTable() {
 							<TableRow>
 								<TableCell>Pagador</TableCell>
 								<TableCell>Conta</TableCell>
+								<TableCell>Numero do cheque</TableCell>
 								<TableCell>Valor</TableCell>
 								<TableCell>Vencimento</TableCell>
 								<TableCell>Ações</TableCell>
 							</TableRow>
 						</TableHead>
 						<TableBody>
-							{checks.checks.map((row) => (
+							{checks.checks.filter(applyFilters).map((row) => (
 								<TableRow
 									key={row.uid}
 									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
@@ -48,6 +209,7 @@ export default function ChecksTable() {
 									>
 										{row.AccountBank.name}
 									</TableCell>
+									<TableCell>{row.checkNumber}</TableCell>
 									<TableCell>{row.value}</TableCell>
 									<TableCell>{formatDate(row.dueDate)}</TableCell>
 									<TableCell className="flex">
